ec8ba8f72d4717ff82e1efa0a65f564f1e9b83ff
NathansinDrake <enrique.luna99359@gmail.com>
2024-09-02 00:57:46 -0500

    primer commit

---
+++ tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
